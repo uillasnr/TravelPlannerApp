d212623ded923c1e889cc8148d4275a7e07e3e40
uillasnr <wiliasreis@hotmail.com>
2024-07-10 20:30:50 -0300

    init: initial project setup

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className=" text-zinc-400">Hellow uillas</Text>
+    </View>
+  );
+}
