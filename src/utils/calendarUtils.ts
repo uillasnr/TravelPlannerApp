46361a460e22fc4a7c6be897f3fa6b99286e59a7
uillasnr <wiliasreis@hotmail.com>
2024-07-14 18:52:47 -0300

    feat: add componente calendar

--- src/utils/calendarUtils.ts
+++ src/utils/calendarUtils.ts
@@ -1,4 +1,4 @@
-/* import dayjs, { Dayjs } from "dayjs"
+ import dayjs, { Dayjs } from "dayjs"
 import { DateData, CalendarUtils } from "react-native-calendars"
 import { MarkedDates } from "react-native-calendars/src/types"
 
@@ -104,4 +104,4 @@ export const calendarUtils = {
   orderStartsAtAndEndsAt,
   formatDatesInText,
   dateToCalendarDate: CalendarUtils.getCalendarDateString,
-} */
+} 
